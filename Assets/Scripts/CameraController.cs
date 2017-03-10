7f017f6013e463f3f0c5da433a602c357b9ba827
Charles Calder <cc9431@bard.edu>
2017-03-10 17:42:38 -0500

    First Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Transform player;
+	private Vector3 lookUp;
+	private Vector3 offset;
+
+	public float turnSpeed;
+
+	void Start () {
+		offset = transform.position - player.transform.position;
+		lookUp = Vector3.up * 2;
+	}
+
+	void LateUpdate()
+	{
+		offset = Quaternion.AngleAxis (Input.GetAxis ("Mouse X") * turnSpeed, lookUp) * offset;
+
+		transform.position = player.position + offset;
+		transform.LookAt (player.position + lookUp);
+	}
+}
