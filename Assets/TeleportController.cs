af3160a30879dd7b5b709c161946c4f0aaa01b41
Ben <bs6171@bard.edu>
2017-03-12 19:13:00 -0400

    Implemented One-Direction Portals
    working

---
+++ Assets/TeleportController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour {
+
+    private GameObject teleportEnter;
+
+    private Transform teleportDest;
+
+	// Use this for initialization
+	void Start () {
+        teleportEnter = GameObject.Find("TeleportEnter");
+        teleportDest = GameObject.Find("TeleportDest").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider Other)
+    {
+        if (Other.gameObject.CompareTag("Player"))
+        {
+            Other.GetComponent<Rigidbody>().transform.position = new Vector3(teleportDest.position.x, teleportDest.position.y, teleportDest.position.z);
+        }
+    }
+}
