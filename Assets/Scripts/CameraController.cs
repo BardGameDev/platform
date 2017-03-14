4fd8fb50e84d06b38a4e61a2175a9937526a07d7
Charles Calder <cc9431@bard.edu>
2017-03-14 17:52:05 -0400

    Changed Mouse X axis to CameraAxis also started working on the ability to look higher up

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,7 +6,9 @@ public class CameraController : MonoBehaviour {
 	private Transform player;
 	private Vector3 lookUp;
 	private Vector3 offset;
+	private Vector3 lookMoreUp;
 
+	private float lookUpSpeed;
 	public float turnSpeed;
 
 	void Start () {
@@ -17,7 +19,16 @@ public class CameraController : MonoBehaviour {
 
 	void LateUpdate() {
 		//This spins the offset around the player when the left or right keys are pressed, the "Mouse X" axis is just a place holder for now
-		offset = Quaternion.AngleAxis (Input.GetAxis ("Mouse X") * turnSpeed, lookUp) * offset;
+		offset = Quaternion.AngleAxis (Input.GetAxis ("CameraAxis") * turnSpeed, lookUp) * offset;
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			lookMoreUp = Vector3.up * 6;
+		} else {
+			lookMoreUp = lookUp;
+		}
+
+		lookUp = Vector3.Lerp(lookUp, lookMoreUp, Time.deltaTime * lookUpSpeed); //Lerp function with Time.delta creates a smooth transition every frame to the final destination
+
 
 		transform.position = player.position + offset; //Reset the position based on the player movement and Mouse X input
 		transform.LookAt (player.position + lookUp); //Turn the camera towards the player, but don't look directly down at player
