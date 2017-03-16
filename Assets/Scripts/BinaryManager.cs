5b7b1cd6636c690d973f0537449e5cc1b8442408
Charles Calder <cc9431@bard.edu>
2017-03-15 20:19:43 -0400

    Started merging Have elias, Charlie, and the lobby done

---
+++ Assets/Scripts/BinaryManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinaryManager : MonoBehaviour {
+
+	bool[] puzzle = new bool[6];
+	bool [] ans = new bool[6];
+
+	private int count;
+	private bool isDone;
+
+	void Start(){
+		for (int i = 0; i < puzzle.Length; i++) {
+			puzzle [i] = false;
+			ans [i] = false;
+		}
+		ans [0] = true;
+		ans [3] = true;
+		ans [4] = true;
+		isDone = false;
+	}
+
+	void binary1(){
+		puzzle [0] = puzzle [0] == false;
+		print ("1");
+	}
+	void binary2(){
+		puzzle [1] = puzzle [1] == false;
+		print ("2");
+	}
+	void binary4(){
+		puzzle [2] = puzzle [2] == false;
+		print ("4");
+	}
+	void binary8(){
+		puzzle [3] = puzzle [3] == false;
+		print ("8");
+	}
+	void binary16(){
+		puzzle [4] = puzzle [4] == false;
+		print ("16");
+	}
+	void binary32(){
+		puzzle [5] = puzzle [5] == false;
+		print ("32");
+	}
+
+	void Update(){
+		if(!isDone){
+		 	count = 0;
+			for (int i = 0; i < puzzle.Length; i++) {
+				if (puzzle [i] == ans [i]) {
+					count++;
+				}
+			}
+		}
+		if (count == 6) {
+			//bring down platform
+			//activate pickup object
+			isDone = true;
+			Vector3 final_pos = new Vector3(gameObject.transform.position.x, -11.5f,gameObject.transform.position.z);
+			gameObject.transform.position = Vector3.Lerp (gameObject.transform.position, final_pos, Time.deltaTime);
+		}
+	}
+}
